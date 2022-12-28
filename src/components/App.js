ba06ee23ee450eef816456363493ec2aa7747bfb
Ksu <oksana.ivanova.fd@gmail.com>
2022-12-28 14:27:00 +0200

    file and folder structure

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+
+import useStyles from './styles';
+
+import { Actors, MovieInformation, Movies, NavBar, Profile } from './index';
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route path="/movie/:id" element={<MovieInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/" element={<Movies />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
+
+export default App;
