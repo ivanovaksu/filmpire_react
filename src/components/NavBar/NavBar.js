4175d30a9cc61157dda32ca4d0a2432f76638aac
Ksu <oksana.ivanova.fd@gmail.com>
2022-12-29 14:55:04 +0200

    layout of Navbar and Sidebar components implemented

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery } from '@mui/material';
+import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles';
 
-const NavBar = () => (
-  <div>
-    NavBar
-  </div>
-);
+import { Sidebar } from '..';
+import useStyles from './styles';
+
+const NavBar = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width:600px)');
+  const theme = useTheme();
+  const isAuthenticated = true;
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  return (
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
+          <IconButton
+            color="inherit"
+            edge="start"
+            style={{ outline: 'none' }}
+            onClick={() => setMobileOpen((prev) => !prev)}
+            className={classes.menuButton}
+          >
+            <Menu />
+          </IconButton>
+          )}
+          <IconButton
+            color="inherit"
+            sx={{ ml: 1 }}
+            onClick={() => {}}
+          >
+            {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+          {!isMobile && 'Search...'}
+          <div>
+            {!isAuthenticated ? (
+              <Button color="inherit" onClick={() => {}}>
+                Login &nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button
+                color="inherit"
+                component={Link}
+                to="/profile/:id"
+                className={classes.linkButton}
+                onClick={() => {}}
+              >
+                {!isMobile && <>My Movies &nbsp;</>}
+                <Avatar
+                  style={{ width: 30, height: 30 }}
+                  alt="Profile"
+                  src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+                />
+              </Button>
+            )}
+          </div>
+          {isMobile && 'Search...'}
+        </Toolbar>
+      </AppBar>
+      <div>
+        <nav className={classes.drawer}>
+          {isMobile ? (
+            <Drawer
+              variant="temporary"
+              anchor="right"
+              open={mobileOpen}
+              onClose={() => setMobileOpen((prev) => !prev)}
+              classes={{ paper: classes.drawerPaper }}
+              ModalProps={{ keepMounted: true }}
+            >
+              <Sidebar setMobileOpen={setMobileOpen} />
+            </Drawer>
+          ) : (
+            <Drawer
+              classes={{ paper: classes.drawerPaper }}
+              variant="permanent"
+              open
+            >
+              <Sidebar setMobileOpen={setMobileOpen} />
+            </Drawer>
+          )}
+        </nav>
+      </div>
+    </>
+  );
+};
 
 export default NavBar;
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { default as Movies } from './Movies/Movies';
 export { default as MovieInformation } from './MovieInformation/MovieInformation';
 export { default as NavBar } from './NavBar/NavBar';
 export { default as Profile } from './Profile/Profile';
+export { default as Sidebar } from './Sidebar/Sidebar';
+
